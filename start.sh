47f18a982c48d909670e311ed0b7a12cea48887c
AviSoomirtee <avisoomirtee.as@gmail.com>
2018-07-11 10:57:21 +0000

    New scripts

---
+++ start.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+echo "*******************"
+echo "*                 *"
+sleep 2
+echo "Setup starting...."
+sleep 3
+echo "System check..."
+sleep 5
+echo "Dumping trash files..."
+sleep 5
+echo "Initializing cleanup..."
+sleep 10
+
+yum check-update
+
+echo "System is update..."
+echo "Checking network configurations"
+sleep 3
+ip a s
+echo " Cheking open ports"
+sleep 5
+echo "Installing netstat..."
+sleep 5
+echo "Installation done..."
+sleep 3
+
+echo "Setup completed successfull!"
+sleep 2
+echo "*                 *"
+echo "*******************"
+echo "Rebooting"
+cat LPG-LinuxGuide.txt >> /.bashrc
+sleep 2
+reboot
+
